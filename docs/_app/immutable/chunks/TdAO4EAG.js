433ca675a1a5c77f49106ec06fc582fd62050581
Team Cardinalis <killianott4@gmail.com>
2025-09-04 14:47:13 +0200

    update

--- docs/_app/immutable/chunks/TdAO4EAG.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_ctt67p?.base??"/CV",t=globalThis.__sveltekit_ctt67p?.assets??s;export{t as a,s as b};
---
+++ docs/_app/immutable/chunks/hUtxuALO.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_11v83ap?.base??"/CV",a=globalThis.__sveltekit_11v83ap?.assets??s;export{a,s as b};
